c866054454290c46fab7adeb9e684a022aa53718
gabdesapf <95264701+gabdesapf@users.noreply.github.com>
2021-11-29 21:28:54 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBLSd009Ra74u8eXWr4br9-Z4Nr2wA2y7k",
+  authDomain: "gabdesapf-abed6.firebaseapp.com",
+  projectId: "gabdesapf-abed6",
+  storageBucket: "gabdesapf-abed6.appspot.com",
+  messagingSenderId: "261866210407",
+  appId: "1:261866210407:web:d52a260606748be338c0c2"
+
+});
